0de11eea8d2922898dbae1d0b3b392c73d97a6a5
Malik Almohsen <malik@Maliks-MacBook-Pro-2.local>
2020-12-01 11:50:49 -0800

    Made initial server setup

---
+++ server.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 80;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res, next) {
+  res.status(200).render('homePage');
+});
+
+app.get('/index.html', function(req, res, next) {
+  res.status(200).render('homePage');
+});
+
+app.get('/trip.html', function(req, res, next) {
+  res.status(200).render('trip');
+});
+
+app.get('/gallery.html', function(req, res, next) {
+  res.status(200).render('gallery');
+});
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.status(404).render('404', {
+    url: req.url
+  });
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
