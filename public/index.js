f99cd67567df67ab9127aefbac25949797a2b8b7
Malik Almohsen <malik@Maliks-MacBook-Pro-2.local>
2020-12-09 16:27:47 -0800

    Moved code

--- public/index.js
+++ public/index.js
@@ -1,16 +1,2 @@
 //This .js file contains javascript for various functions within /trip that are unrelated to the map (pretty much just a couple buttons for now)
-
-var x = document.getElementById("button-close");
-var cancel = document.getElementById("cancel-trip-button");
-
-function removeLocation() {
-
-var location = document.getElementById("results");
-location.innerHTML = "";
-
-x.style.display = "none"
-document.getElementById("trip-plan-forum").style.display = "none"
-}
-
-x.addEventListener("click", removeLocation);
-cancel.addEventListener("click", removeLocation);
+//Malik: moved contents to trip.js since this should be for index.html/homepage
