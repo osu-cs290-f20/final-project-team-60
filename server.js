638770cde63581fba0ca15ed4a0738705f4f051f
Malik Almohsen <malik@Maliks-MacBook-Pro-2.local>
2020-12-10 13:39:09 -0800

    Edited to work with templates and partials

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var tripsData = require('./tripsData.json');
+// console.log(tripsData[0].images);
 
 var app = express();
 var port = process.env.PORT || 80;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: null}));
 app.set('view engine', 'handlebars');
+
+// var imageEntery = {
+//   photoURL: '',
+//   caption: ''
+// }
+
+function imageEntery(photoURL, caption) {
+  this.photoURL = photoURL;
+  this.caption = caption;
+}
+
+function getImages() {
+  var combinedImagesArray = [];
+
+  for (var i = 0; i < tripsData.length; i++) {
+    for (var j = 0; j < tripsData[i].images.length; j++) {
+      combinedImagesArray = combinedImagesArray.concat(new imageEntery(tripsData[i].images[j], tripsData[i].tripTitle));
+    }
+  }
+
+  return combinedImagesArray;
+}
+
+var imagesArray = getImages();
+console.log(imagesArray);
+
 
 app.get('/', function(req, res, next) {
   res.status(200).render('homePage');
@@ -14,7 +42,8 @@ app.get('/', function(req, res, next) {
 
 app.get('/index.html', function(req, res, next) {
   res.status(200).render('homePage', {
-    pagetitle: "Home"
+    // pagetitle: "Home"
+    tripData: tripsData
   });
 });
 
@@ -27,7 +56,9 @@ app.get('/trip.html', function(req, res, next) {
 
 app.get('/gallery.html', function(req, res, next) {
   res.status(200).render('gallery', {
-    pagetitle: "Gallery"
+    // pagetitle: "Gallery",
+    photos: imagesArray
+    // caption: "Some photo"
   });
 });
 
