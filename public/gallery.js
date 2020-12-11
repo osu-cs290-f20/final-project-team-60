677a89e96b57b8d0de23d81f83236bfa01edb4b9
allisonrenie <allisonrenie@gmail.com>
2020-12-11 03:44:21 -0800

    adding more trip data to the json file and cleaning up code

--- public/gallery.js
+++ public/gallery.js
@@ -1,12 +1,10 @@
 window.onload = function(){
-  // slideshow stuff
   var index = 1;
   hideSlides();
   showSlide(0);
 
   function moveSlide(a) {
     hideSlides();
-    // var slideToShow = index + a;
     showSlide(index += a);
   }
 
@@ -15,17 +13,11 @@ window.onload = function(){
     for (var i = 0; i < slides.length; i++){
 
       slides[i].style.display = "none";
-      //slides[i].toggle();
-      // console.log(slides.item(0));
-      //slides[i].toggle();
     }
   }
 
   function showSlide(input){
-    // console.log(thisSlide);
     var slides = document.getElementsByClassName("photo-post");
-    //console.log(slides.length);
-    // var index = thisSlide;
     if (Number(input) > Number(slides.length - 1)){
       console.log("hi");
       index = 1;
@@ -39,8 +31,6 @@ window.onload = function(){
 
 
     slides[index].style.display = "block";
-    // slides[thisSlide].style.display = "block";
-    //slides[thisSlide].toggle();
   }
 
   var prevButton = document.getElementById("previous-button");
@@ -55,37 +45,6 @@ window.onload = function(){
     moveSlide(1);
   });
 
+// I wrote this file with help from the W3School's article: https://www.w3schools.com/howto/howto_js_slideshow.asp
 
-//   // showSlides(index);
-//   //
-//   // // Next/previous controls
-//   // function plusSlides(n) {
-//   //   showSlides(index += n);
-//   // }
-//   //
-//   // // // Thumbnail image controls
-//   // // function currentSlide(n) {
-//   // //   showSlides(slideIndex = n);
-//   // // }
-//   //
-//   // function showSlides(n) {
-//   //   var i;
-//   //   var slides = document.getElementsByClassName("photo-post");
-//   //   // var dots = document.getElementsByClassName("dot");
-//   //   if (n > slides.length){
-//   //     slideIndex = 1
-//   //   }
-//   //   if (n < 1){
-//   //     slideIndex = slides.length
-//   //   }
-//   //   for (i = 0; i < slides.length; i++) {
-//   //       slides[i].style.display = "none";
-//   //   }
-//   //   for (i = 0; i < dots.length; i++) {
-//   //       dots[i].className = dots[i].className.replace(" active", "");
-//   //   }
-//   //   slides[slideIndex-1].style.display = "block";
-//   //   dots[slideIndex-1].className += " active";
-//   // }
-//
 };
